de393d404b52fcda9ca422ac6f01041cd07b11f5
Javier Ramón <javierramon@outlook.com>
2017-11-29 16:33:07 +0100

    Mas y mas comentarios.

--- nodepop/routes/anuncios.js
+++ nodepop/routes/anuncios.js
@@ -1,12 +1,20 @@
+// Para EJECUTAR el código JS en 'modo estricto', que es menos 'flexible' con los errores de sintaxis de JS.
 'use strict';
 
+/**
+ * Se cargan los MODULOS necesarios para la app.
+ */
 const router = require('express').Router();
 const fs = require('fs');
+// Se carga el Modelo ANUNCIO de Mongoose para trabajar con la BD.
 const Anuncio = require('mongoose').model('Anuncio');
 
-/* GET anuncios page. */
+/* Petición GET a la página de Anuncios. */
+// Se utiliza ASYNC/AWAY por que vamos a trabajar con la BD (operaciones ASINCRONAS).
 router.get('/', async function (req, res, next) {
+  // Los bloques TRY/CATCH permiten tratar los posibles ERRORES que se produzcan durante la ejecución de la función.
   try {
+    // Se leen los POSIBLES PARAMETROS de ENTRADA que vienen en una QUERY STRING.
     const start = parseInt(req.query.start) || 0;
     const limit = parseInt(req.query.limit) || 1000; // nuestro api devuelve max 1000 registros
     const sort = req.query.sort || '_id';
@@ -20,9 +28,13 @@ router.get('/', async function (req, res, next) {
       filters.venta = req.query.venta;
     }
 
+    // Se realiza una CONSULTA a la BD, esto es una operación ASINCRONA.
     const {total, rows} = await Anuncio.list(filters, start, limit, sort, includeTotal);
     res.render('anuncios', { total, anuncios: rows });
   } catch(err) { return res.next(err); }
 });
 
+/**
+ * Se EXPORTA la RUTA para poder utilizarla en otro fichero (concretamente en app.js).
+ */
 module.exports = router;
--- nodepop/routes/index.js
+++ nodepop/routes/index.js
@@ -6,32 +6,40 @@
  */
 /**
  * Método Router() de Express:
- * 
+ * Define RUTAS que pueden solicitarse a la APP.
+ * Por lo general estarán asociadas a una función que se ejecutará cuando la PETICION COINCIDA con la RUTA.
  */
 const router = require('express').Router();
 const fs = require('fs');
 const path = require('path');
 
-/* Peticion GET al Home. */
+/**
+ * Peticion GET al Home.
+ * Notar que en la PETICIONES NO SE TIENE EN CUENTA el DOMINIO PRINCIPAL.
+ * Es decir: 'www.una_web_cualquiera.com/' es una PETICION a la RAIZ del sitio web '/'.
+ */
+// Se utiliza ASYNC/AWAY por que vamos a trabajar con ficheros (operaciones ASINCRONAS).
 router.get('/', async function (req, res, next) {
-  //
+  // Los bloques TRY/CATCH permiten tratar los posibles ERRORES que se produzcan durante la ejecución de la función.
   try {
-    //
+    // Guardamos la ruta del fichero README.md
     const filename = path.join(__dirname, '../README.md');
-    //
-    const readme = await new Promise((res, rej) => 
-      fs.readFile(filename, 'utf8', (err, data) => err ? rej(err) : res(data) )
+    // Se lee el fichero y su contenido se guarda en la constante 'readme'.
+    // Como es una operación asíncrona utilizamos una PROMESA.
+    const readme = await new Promise((resolve, rejected) => 
+      fs.readFile(filename, 'utf8', (err, data) => err ? rejected(err) : resolve(data) )
     );
-    //
+    // Se RENDERIZA la VISTA del HOME (index.ejs).
+    // a la que se la pasa la constante 'readme' para que la muestre.
     res.render('index', { readme });
-  //
+  //Si se produce un ERROR...
   } catch (err) { 
-      //
+      // 'SALTAMOS' al middleware de MANEJO de ERRORES.
       return next(err); 
   }
 });
 
 /**
- * 
+ * Se EXPORTA la RUTA para poder utilizarla en otro fichero (concretamente en app.js).
  */
 module.exports = router;
